0b50357ceab54a8b0e340604fb6bf1c10ebcff02
josh <joshkenmarks@gmail.com>
2026-06-29 09:28:59 +0200

    Create task4.asm

---
+++ linux/task4.asm
@@ -0,0 +1,56 @@
+; 32-bit Linux assembly
+        global  _start
+        
+        section .text
+_start: mov     eax, [group_A]
+
+        xor     edx, edx    ; clear edx for division
+        mov     ecx, 3      ; dividing by 3 per group
+        div     ecx         ; eax divide by ecx
+        
+        mov     edi, buffer_end
+        dec     edi                     ; point to next position in buffer
+        
+_loop:  ; convert to ASCII
+        xor     edx, edx    ; clear edx
+        mov     ecx, 10
+        div     ecx         ; eax divide by ecx
+        
+        add     dl, '0'     ; lowest byte of edx convert to ASCII
+        mov     [edi], dl   ; store ASCII in buffer
+        dec     edi         ; point to next position in buffer
+        
+        test    eax, eax    ; check equals 0
+        jnz     _loop
+        
+        inc     edi         ; move pointer back to the start of the answer
+        
+_print: mov     eax, 4          ; write system call
+        mov     ebx, 1          ; output to stdout
+        mov     ecx, msg        ; move address of msg into ecx register
+        mov     edx, msglen     ; move number of characters in message into edx register
+        int     0x80            ; interrupts the CPU to allow the kernel to process system call
+        
+        mov     eax, 4          ; write system call
+        mov     ebx, 1          ; output to stdout
+        mov     ecx, edi        ; start of the ASCII answer
+        mov     edx, buffer_end ; end of the ASCII answer
+        sub     edx, edi        ; edx = buffer_end - start (calculates exact character count)
+        int     0x80            ; interrupts the CPU to allow the kernel to process system call
+        
+_exit:  mov     eax, 1          ; system exit
+        int     0x80
+
+        section .data
+group_A:        dd      60
+group_B:        dd      98
+group_C:        dd      59
+group_D:        dd      43
+group_E:        dd      15
+group_F:        dd      57
+msg:            db      "Total number of groups: "
+msglen: equ     $ - msg         ; length of message
+
+        section .bss
+buffer:         resb    12          ; Reserve 12 bytes for the number and newline
+buffer_end:
