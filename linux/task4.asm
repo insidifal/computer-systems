60c4a58b3c39f89edd19f31984fcff5ae5bfdbb6
josh <joshkenmarks@gmail.com>
2026-06-29 10:02:01 +0200

    Update task4.asm

--- linux/task4.asm
+++ linux/task4.asm
@@ -2,16 +2,30 @@
         global  _start
         
         section .text
-_start: mov     eax, [group_A]
+_start: mov     esi, group_A    ; point to start of memory address
+        mov     [current_group], byte 'A'
+        
+_loop1:
+        mov     eax, [esi]  ; put value into accumulator
 
         xor     edx, edx    ; clear edx for division
         mov     ecx, 3      ; dividing by 3 per group
         div     ecx         ; eax divide by ecx
         
+        ; check for remainder
+        test    edx, edx
+        jz      _fill_buffer
+        inc     eax
+        
+_fill_buffer:
         mov     edi, buffer_end
         dec     edi                     ; point to next position in buffer
+    
+_newline:
+        mov     [edi], byte 10
+        dec     edi
         
-_loop:  ; convert to ASCII
+_loop2:  ; convert to ASCII
         xor     edx, edx    ; clear edx
         mov     ecx, 10
         div     ecx         ; eax divide by ecx
@@ -21,11 +35,15 @@ _loop:  ; convert to ASCII
         dec     edi         ; point to next position in buffer
         
         test    eax, eax    ; check equals 0
-        jnz     _loop
+        jnz     _loop2
         
         inc     edi         ; move pointer back to the start of the answer
         
-_print: mov     eax, 4          ; write system call
+_print: ; update letter
+        mov     al, [current_group]
+        mov     [msg_letter], al
+
+        mov     eax, 4          ; write system call
         mov     ebx, 1          ; output to stdout
         mov     ecx, msg        ; move address of msg into ecx register
         mov     edx, msglen     ; move number of characters in message into edx register
@@ -38,6 +56,12 @@ _print: mov     eax, 4          ; write system call
         sub     edx, edi        ; edx = buffer_end - start (calculates exact character count)
         int     0x80            ; interrupts the CPU to allow the kernel to process system call
         
+_next_group:
+        add     esi, 4                      ; next 4 byte integer
+        inc     byte [current_group]        ; next letter
+        cmp     byte [current_group], 'F'   ; check if done
+        jbe     _loop1
+        
 _exit:  mov     eax, 1          ; system exit
         int     0x80
 
@@ -48,9 +72,11 @@ group_C:        dd      59
 group_D:        dd      43
 group_E:        dd      15
 group_F:        dd      57
-msg:            db      "Total number of groups: "
+msg:            db      "Mini groups in group "
+msg_letter:     db      "A: "
 msglen: equ     $ - msg         ; length of message
 
         section .bss
+current_group:  resb    1
 buffer:         resb    12          ; Reserve 12 bytes for the number and newline
 buffer_end:
